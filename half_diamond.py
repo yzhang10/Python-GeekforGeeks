691bda7843941bc1e36c5914513507ac43557c4a
yzhang10 <42006065+yzhang10@users.noreply.github.com>
2020-08-20 14:05:18 -0400

    Update half_diamond.py

--- half_diamond.py
+++ half_diamond.py
@@ -1,8 +1,8 @@
 ## https://www.geeksforgeeks.org/program-to-print-half-diamond-star-pattern/?ref=leftbar-rightbar
 def half_diamond(N):
-  for i in range(2*N):
+  for i in range(1,2*N+1):
     if i + 1 <= N:
-		print("*" * (i+1))
-	else:
-		i=2*N-1-i
-		print("*" * i)
+        print("*" * i)
+    else:
+        i=2*N-i
+        print("*" * i)
